d5ddda9ebb4403068edb4760c22e4ab456aadc36
k-kaps <karan.kapoor@uwaterloo.ca>
2025-07-27 20:47:47 -0400

    * added fifo test * updated readme

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # IMU Driver
 
-Developing a driver for the MPU 6050 IMU for Linux.
+A user-space driver for the MPU 6050 IMU for Linux.
+
+### File Structure
+This project has been structured as follows.
+```
+imu-driver
+├── include
+│   ├── helpers.hpp
+│   └── mpu6050_driver.hpp
+├── src
+│   └── mpu6050_driver.cpp
+├── test
+│   └── test.cp
+├── CMakeLists.txt
+└── README.md
+```
+The `mpu6050_driver.hpp` file contains the `MPU6050Driver` class and its member functions, all the helper macros, structs, and enums have been defined in the `helpers.hpp` file.
+
+### Build
+This project builds a library for the IMU and a small test executable which tests basic functionality of capturing accelerometer and gyroscope values, and testing the IMU's FIFO buffer. The project can be built by:
+
+```bash
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
+
+### Resources
+- [MPU-6050 Datasheet](https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf)
+- [MPU-6050 Register Maps](https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf)
--- test/test.cpp
+++ test/test.cpp
@@ -6,7 +6,7 @@ bool test_read_accl() {
 	imu.init_driver();
 	
 	AcclStamped accl_stamped;
-	if(imu.read_accl(accl_stamped)) {
+	if (imu.read_accl(accl_stamped)) {
 		std::cout << "Timestamp : " << accl_stamped.timestamp << std::endl;
 
 		std::cout << "Accelerometer Data : " << std::endl;
@@ -25,7 +25,7 @@ bool test_read_gyro() {
 	imu.init_driver();
 	
 	AcclStamped gyro_stamped;
-	if(imu.read_accl(gyro_stamped)) {
+	if (imu.read_accl(gyro_stamped)) {
 		std::cout << "Timestamp : " << gyro_stamped.timestamp << std::endl;
 
 		std::cout << "Accelerometer Data : " << std::endl;
@@ -44,7 +44,7 @@ bool test_read_imu() {
 	imu.init_driver();
 
 	IMUStamped imu_stamped;
-	if(imu.read_imu(imu_stamped)) {
+	if (imu.read_imu(imu_stamped)) {
 		std::cout << "Timestamp : " << imu_stamped.timestamp << std::endl;
 
 		std::cout << "Accelerometer Data : " << std::endl;
@@ -62,6 +62,42 @@ bool test_read_imu() {
 	return false;
 }
 
+bool test_fifo() {
+	MPU6050Driver imu = MPU6050Driver();
+
+	imu.init_driver();
+
+	if (imu.get_fifo_status()) {
+		return false;
+	}
+
+	imu.set_fifo_enabled(true, FifoCFG::ACCL);
+
+	if (!imu.get_fifo_status()) {
+		return false;
+	}
+
+	usleep(500'000);
+	uint8_t buf[50];
+
+	if (!imu.read_fifo_buffer(buf, 50)) {
+		return false;
+	}
+
+	for (int i = 0; i < 25; i++) {
+		int16_t val = (buf[2*i] << 8) | (buf[2*i + 1] & 0xFF);
+		double accl_val = val * GRAVITY / 16384.0;
+		std::cout << "VAL " << (i + 1) << ": " << accl_val << std::endl;
+	}
+
+	imu.set_fifo_enabled(false, FifoCFG::FIFO_NONE);
+
+	if (imu.get_fifo_status()) {
+		return false;
+	}
+	return true;
+}
+
 int main() {
 	std::cout << "[TEST 1] : READ ACCELEROMETER DATA" << std::endl;
 	std::string result1 = test_read_accl() ? "PASSED" : "FAILED";
@@ -74,4 +110,8 @@ int main() {
 	std::cout << "[TEST 3] : READ IMU DATA" << std::endl;
 	std::string result3 = test_read_accl() ? "PASSED" : "FAILED";
 	std::cout << "[TEST 3] : " << result3 << std::endl;
+
+	std::cout << "[TEST 4] : READ FROM FIFO BUFFER" << std::endl;
+	std::string result4 = test_fifo() ? "PASSED" : "FAILED";
+	std::cout << "[TEST 4] : " << result4 << std::endl;
 }
